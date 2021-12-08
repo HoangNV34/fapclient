e3499a3cf3d62eadbae73043654cda634175a44e
Hoang <hoangnvhe130834@fpt.edu.vn>
2021-12-08 14:28:15 +0700

    Initial Create, Config database, Create CoreRepository

---
+++ FapClient/FapClient.Core/Repository/CoreRepository.cs
@@ -0,0 +1,73 @@
+﻿using FapClient.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FapClient.Core.Repository
+{
+    public class CoreRepository<T> : ICoreRepository<T> where T : class
+    {
+        protected readonly AP2Context _context = new AP2Context();
+        private readonly DbSet<T> dbSet;
+
+        public CoreRepository()
+        {
+            var typeOfDbSet = typeof(DbSet<T>);
+            foreach (var prop in _context.GetType().GetProperties())
+            {
+                if (typeOfDbSet == prop.PropertyType)
+                {
+                    dbSet = prop.GetValue(_context, null) as DbSet<T>;
+                    break;
+                }
+            }
+
+            if (dbSet == null)
+            {
+                dbSet = _context.Set<T>();
+            }
+        }
+
+        public int Add(T entity)
+        {
+            dbSet.Add(entity);
+            return _context.SaveChanges();
+        }
+
+        public int AddRange(List<T> entities)
+        {
+            dbSet.AddRange(entities);
+            return _context.SaveChanges();
+        }
+
+        public bool Delete(T entity)
+        {
+            dbSet.Remove(entity);
+            return _context.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            if (id > 0)
+            {
+                dbSet.Remove(GetById(id));
+                return _context.SaveChanges() > 0;
+            }
+            return false;
+        }
+
+        public T GetById(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public bool Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
---
+++ FapClient/FapClient.Core/Repository/ICoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace FapClient.Core.Repository
+{
+    public interface ICoreRepository<T>
+    {
+        T GetById(int id);
+
+        int Add(T entity);
+
+        bool Delete(T entity);
+
+        bool Update(T entity);
+
+        bool Delete(int id);
+
+        int AddRange(List<T> entities);
+    }
+}
